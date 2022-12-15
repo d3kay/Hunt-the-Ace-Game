47abda8ed4468771b4416d82a859e9ff3ed78435
d3kay <fahim.anwar1997@gmail.com>
2022-12-15 15:48:19 +0100

    modifications in html & css

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Hunt-the-Ace-Game
 
-A Browser based Card game by following this tutorial 
+A Browser based Card game made by following this tutorial 
 
 ----- https://youtu.be/Bj6lC93JMi0
---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+<div class="card">
+<div class="card-inner">
+    <div class="card-front">
+        <img src="/images/card-JackClubs.png" alt="" class="card-img">
+    </div>
+    <div class="card-back">
+        <img src="/images/card-back-Blue.png" alt="" class="card-img">
+    </div>
+</div>
+</div>
+*/
